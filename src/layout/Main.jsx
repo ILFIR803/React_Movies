27949bc677bbe77133f61a5f90955246ca963824
Ilfir <ilfir803@gmail.com>
2023-10-31 15:22:12 +0500

    update

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,7 +3,7 @@ import { Movies } from '../components/Movies'
 import { Preloader } from '../components/Preloader'
 import { Search } from '../components/Search'
 
-const API_KEY=process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class Main extends Component {
    state = {
@@ -16,32 +16,32 @@ class Main extends Component {
    componentDidMount() {
       fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
          .then((response) => response.json())
-         .then((data) => 
+         .then((data) =>
             this.setState({ movies: data.Search, loading: false }))
-         .catch(() => {
+         .catch((err) => {
             console.error(err);
-            this.setState({ loading: false});
+            this.setState({ loading: false });
          });
    }
-   searchMovies = (str, type="all") => {
+   searchMovies = (str, type = "all") => {
       fetch(
          `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== "all" ? `&type=${type}` : ''}`)
-            .then(response => response.json())
-            .then(data => this.setState({ movies: data.Search }));
-            .catch(() => {
-               console.error(err);
-               this.setState({ loading: false});
-            });
+         .then(response => response.json())
+         .then(data => this.setState({ movies: data.Search }));
+            .catch ((err) => {
+         console.error(err);
+         this.setState({ loading: false });
+      });
    }
    render() {
       const { movies, loading } = this.state;
       return <main className="container content">
          <Search searchMovies={this.searchMovies} />
          {
-            loading ? 
-            (<Preloader />) 
-            : 
-            (<Movies movies={movies} />)
+            loading ?
+               (<Preloader />)
+               :
+               (<Movies movies={movies} />)
          }
 
       </main>
